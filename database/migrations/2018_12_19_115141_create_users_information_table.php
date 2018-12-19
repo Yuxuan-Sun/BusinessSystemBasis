0c20b63b61ed2b9285c499323252847876020b49
johanna sun <sunyx.johanna2016@outlook.com>
2018-12-19 23:15:08 +0800

    user-info-table

---
+++ database/migrations/2018_12_19_115141_create_users_information_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersInformationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_information', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->longText('description');
+            $table->integer('money');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_information');
+    }
+}
