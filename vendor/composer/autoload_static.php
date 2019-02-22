30681bef161cd568470a318a9627080c80c41d44
johanna sun <sunyx.johanna2016@outlook.com>
2019-02-22 19:21:39 +0800

    init

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -440,6 +440,7 @@ class ComposerStaticInitaf024ecba7358f109d455487034f0b55
         'App\\Http\\Middleware\\TrimStrings' => __DIR__ . '/../..' . '/app/Http/Middleware/TrimStrings.php',
         'App\\Http\\Middleware\\TrustProxies' => __DIR__ . '/../..' . '/app/Http/Middleware/TrustProxies.php',
         'App\\Http\\Middleware\\VerifyCsrfToken' => __DIR__ . '/../..' . '/app/Http/Middleware/VerifyCsrfToken.php',
+        'App\\Map' => __DIR__ . '/../..' . '/app/Map.php',
         'App\\Providers\\AppServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AppServiceProvider.php',
         'App\\Providers\\AuthServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AuthServiceProvider.php',
         'App\\Providers\\BroadcastServiceProvider' => __DIR__ . '/../..' . '/app/Providers/BroadcastServiceProvider.php',
